71805bc9ec715a9ff28bf876f2610cb6968b82fc
xiamu <chuhankuangzi@outlook.com>
2023-02-25 20:10:22 +0800

    2010hanshu

--- test.cpp
+++ test.cpp
@@ -2,29 +2,58 @@
 #include <vector>
 using namespace std;
 
+class Box
+{
+   public:
+      double length;
+      double breadth;
+      double height;
+
+      double getVolume(void);
+      void  setLength(double len);
+      void setBreadth(double bre);
+      void setHeight(double hei);
+
+};
+
+double Box::getVolume(void)
+{
+   return length * breadth * height;
+}
+
+void Box::setLength(double len)
+{
+   length = len;
+}
+
+void Box::setBreadth(double bre)
+{
+   breadth = bre;
+}
+
+void Box::setHeight(double hei)
+{
+   height = hei;
+}
+
 int main()
 {
-   vector<int> vec;
-   int i;
+   Box Box1;
+   Box Box2;
+   double volume = 0.0;
 
-   cout << "vector size = " << vec.size() << endl;
-   for(i = 0;i<5;i++)
-   {
-      vec.push_back(i);
-   }
+   Box1.setLength(6.0);
+   Box1.setBreadth(7.0);
+   Box1.setHeight(5.0);
 
-   cout << "vector size = " << vec.size() << endl;
+   Box2.setLength(12.0);
+   Box2.setBreadth(13.0);
+   Box2.setHeight(10.0);
 
-   for(i = 0; i< 5;i++)
-   {
-      cout << " value of vec [" << i << "] = " << vec[i] << endl;
-   }
+   volume = Box1.getVolume();
+   cout << "Box1 = " << volume << endl;
 
-   vector<int>::iterator v = vec.begin();
-   while(v != vec.end())
-   {
-      cout << "value of v = " << *v << endl;
-      v++;
-   }
+   volume = Box2.getVolume();
+   cout << "Box2 = " << volume << endl;
    return 0;
 }
